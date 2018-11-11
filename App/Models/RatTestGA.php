a1a378e0d6ed08a3f229fcd654a20c6775bd8b28
secretsource <secretsource@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2018-11-11 12:41:35 +0000

    Updated readme, added an icon and banner and added one screen shot.
    git-svn-id: http://plugins.svn.wordpress.org/ratify/trunk@1972464 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- App/Models/RatTestGA.php
+++ App/Models/RatTestGA.php
@@ -5,18 +5,19 @@ class RatTestGA extends RatTestBase {
 
     public function __construct( $in = null ) {
         parent::__construct( $in );
-        $this->out['title'] = __( 'Google Analytics', 'ratify' );
+        $this->out['title'] = __( 'Google Analytics / Tag Manager', 'ratify' );
     }
 
     public function runtest( $in = '' ) {
         if ( '' != $in ) {
             $this->in = $in;
         }
-        $res = $this->grep_html( $this->in, '@var _gaq|GoogleAnalyticsObject@ims' );
+        $ga = $this->grep_html( $this->in, '@var _gaq|GoogleAnalyticsObject@ims' );
+        $gtm = $this->grep_html( $this->in, '@googletagmanager\.com|GTM\-@ims' );
 
-        if( 1 == $res['total'] ) {
+        if( 1 == $ga['total'] or 1 == $gtm['total'] ) {
             $this->out['error'] = false;
-            $this->out['data'] = Array( __( 'Google Analytics is installed: ' . $res['out'][0], 'ratify' ) );
+            $this->out['data'] = Array( __( 'Google Analytics or Google Tag Manager is installed: ' . ($ga['total'] > 0 ? $ga['out'][0] : $gtm['out'][0]), 'ratify' ) );
         } else {
             $this->out['error'] = __( 'Google Analytics does not appear to be configured. Please add the Google Analytics code to the site or we won\'t be able to track any user activity. We normally use MonsterInsights to do this.', 'ratify' );
             $this->out['data'] = Array();
