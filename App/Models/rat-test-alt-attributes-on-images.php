6b4931980ba56088b4beb8805823c686253e58a2
Ted Stresen-Reuter <web@secret-source.eu>
2019-05-06 00:15:20 +0100

    PHPCS Fixes

--- App/Models/rat-test-alt-attributes-on-images.php
+++ App/Models/rat-test-alt-attributes-on-images.php
@@ -1,48 +1,70 @@
 <?php
+/**
+ * Ratify Core API: RatTestAltAttributesOnImages class
+ *
+ * @package   Ratify
+ * @author    Ted Stresen-Reuter <ted@secret-source.eu>
+ * @copyright 2018 Secret Source Technology SL
+ * @license   https://github.com/SecretSourceWeb/ratify/blob/master/LICENSE.txt GNU General Public License v2.0
+ */
+
 namespace Ratify\Models;
 
+/**
+ * Checks to see that all images on the page have non-empty ALT attributes.
+ */
 class RatTestAltAttributesOnImages extends RatTestBase {
 
-    public function __construct( $in = null ) {
-        parent::__construct( $in );
-        $this->out['title'] = __( 'IMAGE Elements Have ALT Attributes', 'ratify' );
-    }
+	/**
+	 * Constructor.
+	 *
+	 * @param string $in The HTML to test. Normally the front page.
+	 */
+	public function __construct( $in = null ) {
+		parent::__construct( $in );
+		$this->out['title'] = __( 'IMAGE Elements Have ALT Attributes', 'ratify' );
+	}
 
-    public function runtest( $in = '' ) {
-        parent::runtest( $in );
+	/**
+	 * Runs (is) the actual test.
+	 *
+	 * @param string $in The HTML to test. Normally the front page.
+	 */
+	public function runtest( $in = '' ) {
+		parent::runtest( $in );
 
-        $imagesWithoutAlts = Array();
-        $res = $this->grep_html( $this->in, '@<img (.+?)(?=/>)@ims' );
+		$images_without_alts = array();
+		$res                 = $this->grep_html( $this->in, '@<img (.+?)(?=/>)@ims' );
 
-        if( $res['total'] > 0 ) {
-            // test each image for ALT attributes
-            foreach ($res['out'][1] as $item) {
-                $p = '@alt=(\'|")(.*?)(\'|")@ims';
-                $alts = preg_match($p, $item, $pieces);
-                if ( '' == trim( $pieces[2] ) ) {
-                    preg_match( '@src=(\'|")(.*?)(\'|")@i', $item, $src );
-                    $imagesWithoutAlts[] = admin_url( 'upload.php?item='
-                        . ratify_get_attachment_id_from_src( $src[2] )
-                    );
-                }
-            }
+		if ( $res['total'] > 0 ) {
+			// test each image for ALT attributes.
+			foreach ( $res['out'][1] as $item ) {
+				$p    = '@alt=(\'|")(.*?)(\'|")@ims';
+				$alts = preg_match( $p, $item, $pieces );
+				if ( '' === trim( $pieces[2] ) ) {
+					preg_match( '@src=(\'|")(.*?)(\'|")@i', $item, $src );
+					$images_without_alts[] = admin_url(
+						'upload.php?item='
+						. ratify_get_attachment_id_from_src( $src[2] )
+					);
+				}
+			}
 
-            if( count( $imagesWithoutAlts ) > 0 ) {
-                $this->out['error'] = __( 'Some images were found without ALT attributes.', 'ratify' );
-                $this->out['data'] = $imagesWithoutAlts;
-            } else {
-                $this->out['error'] = false;
-                $this->out['data'] = '';
-            }
+			if ( count( $images_without_alts ) > 0 ) {
+				$this->out['error'] = __( 'Some images were found without ALT attributes.', 'ratify' );
+				$this->out['data']  = $images_without_alts;
+			} else {
+				$this->out['error'] = false;
+				$this->out['data']  = '';
+			}
+		} else {
 
-        } else {
+			$this->out['error'] = __( 'No images were found', 'ratify' );
+			$this->out['data']  = $res['out'][0];
 
-            $this->out['error'] = __( 'No images were found', 'ratify' );
-            $this->out['data'] = $res['out'][0];
-
-        }
-        do_action('ratp_runtest_end');
-        return $this->out;
-    }
+		}
+		do_action( 'ratp_runtest_end' );
+		return $this->out;
+	}
 }
 
